96792fc75629acefaf4de5dfd45112933346f951
Don Pawlowski <dpawlowski@designinginteractive.com>
2013-07-07 18:38:14 -0400

    faked in data is displaying

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 App = Ember.Application.create();
 
+App.Store = DS.Store.extend({
+  adapter: 'DS.FixtureAdapter'
+});
+
 App.Router.map(function() {
   // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return App.ToDo.find();
   }
 });
+
+App.ToDo = DS.Model.extend({
+  task: DS.attr('string'),
+  completed: DS.attr('boolean')
+});
+
+App.ToDo.FIXTURES =[{
+  id: 1,
+  task: "Eat Dinner",
+  completed: false
+}]
